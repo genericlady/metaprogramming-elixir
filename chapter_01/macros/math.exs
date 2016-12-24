4b7e2310ee101bd99e8ae62397b3eb4a28c93b44
genericlady <yiannakokalas@gmail.com>
2016-12-24 17:09:54 -0500

    Move result to it's own line in both macros inside of math.exs

--- chapter_01/macros/math.exs
+++ chapter_01/macros/math.exs
@@ -5,7 +5,8 @@ defmodule Math do
 			lhs = unquote(lhs)
 			rhs = unquote(rhs)
 			result = lhs + rhs
-			IO.puts "#{lhs} plus #{rhs} is #{result}" result
+			IO.puts "#{lhs} plus #{rhs} is #{result}"
+      result
 		end
 	end
 
@@ -15,7 +16,8 @@ defmodule Math do
 			lhs = unquote(lhs)
 			rhs = unquote(rhs)
 			result = lhs * rhs
-			IO.puts "#{lhs} times #{rhs} is #{result}" result
+			IO.puts "#{lhs} times #{rhs} is #{result}"
+      result
 		end
 	end
 end
